11e09f9a43b2da15910172f7b4f4c111b5793d89
Johannes Zellner <webmaster@nebulon.de>
2014-05-14 13:23:12 -0700

    Support SUPERLOG process env to enable the log middleware

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 'use strict';
 
 exports = module.exports = function (prefix) {
+    if (typeof process.env.SUPERLOG === 'undefined') {
+        return function (req, res, next) { next(); };
+    }
+
     return function (req, res, next) {
         if (req.path.indexOf(prefix) === 0) return res.sendfile(req.path.slice(prefix.length));
         next();
